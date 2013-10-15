b8a0a83e3935a42c03541c58212cc4c26eb8feef
ahoward <ara.t.howard@gmail.com>
2013-10-15 14:42:36 -0600

    brand.rb is refactored like a bitch.

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,20 +1,45 @@
 class Brand < MapModel
+#
+  include Naming
+
   attributes :title, :slug, :name, :triggered_send_key, :organization
 
-  def normalize!
-    title, slug, name = %w( title slug name ).map{|attr| get(attr)}
+  normalize do
+    org = self[:organization]
 
-    Util.cases_for(title, slug, name).tap do |cases|
-      self[:title] = cases.title
-      self[:slug] = cases.slug
-      self[:name] = cases.name
+    unless org.nil?
+      organization = Organization.for(org)
+
+      if organization.nil?
+        organization = Organization.create(:title => org)
+      end
+
+      self[:organization] = organization
     end
-
-    self
   end
 
+#
   def rfis
     RFI.where(:brand => slug)
+  end
+
+#
+  class Organization < MapModel
+    include Naming
+
+    attributes :title, :slug, :name
+
+    def normalize!
+      title, slug, name = %w( title slug name ).map{|attr| get(attr)}
+
+      Util.cases_for(title, slug, name).tap do |cases|
+        self[:title] = cases.title
+        self[:slug] = cases.slug
+        self[:name] = cases.name
+      end
+
+      self
+    end
   end
 end
 
