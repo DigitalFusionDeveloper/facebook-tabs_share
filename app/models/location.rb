fa3513844f28d78e2c95fcc2b99d79e8ec4d95db
Spike Ilacqua <spike@indra.com>
2013-10-28 09:27:08 -0600

    Locator functions

--- app/models/location.rb
+++ app/models/location.rb
@@ -110,6 +110,92 @@ class Location
     end
   end
 
+  def Location.find_all_by_zipcode(zipcode, options = {})
+    zipcode = zipcode.to_s.strip.downcase
+    options.to_options!
+
+    locations = []
+
+    unless zipcode.blank?
+      locations = Location.where('zipcode' => zipcode)
+
+      if locations.blank?
+        begin
+          ziploc = GeoLocation.for(zipcode)
+          lat, lng = ziploc.lat, ziploc.lng
+          locations = Location.find_all_by_lng_lat(lng, lat, options)
+        rescue Object => e
+          raise unless Rails.env.production?
+          Rails.logger.error(e)
+        end
+      end
+    end
+
+    locations
+  end
+
+# http://stackoverflow.com/questions/5319988/how-is-maxdistance-measured-in-mongodb
+#   1° latitude = 69.047 miles = 111.12 kilometers
+#
+
+  DEGREES_PER_MILE = 1 / 69.047
+  MILES_PER_DEGREE = 1 / DEGREES_PER_MILE
+
+  def Location.find_all_by_lng_lat(lng, lat, options = {})
+    options.to_options!
+
+    miles = options[:miles] || 100
+    max_distance = miles * DEGREES_PER_MILE
+
+    locations = []
+
+    Location.geo_near([Float(lat),Float(lng)]).max_distance(max_distance).each do |location|
+      location['distance'] = Float(location.geo_near_distance) * MILES_PER_DEGREE
+      locations.push(location)
+    end
+
+    locations
+  end
+
+  def Location.find_all_by_lat_lng(lat, lng, options = {})
+    Location.find_all_by_lng_lat(lng, lat, options = {})
+  end
+
+  def Location.find_all_by_state_and_city(state, city)
+    where(:state => /\A#{ state }\Z/i, :city => /\A#{ city }\Z/i)
+  end
+
+  def Location.find_all_by_state(state)
+    where(:state => /\A#{ state }\Z/i)
+  end
+
+  def Location.find_by_string(string)
+    begin
+      geo = GGeocode.geocode(string)
+    rescue GGeocode::StatusError
+      return []
+    end
+    location = GeoLocation.parse_data(geo)
+    Location.find_all_by_lat_lng(location.lat,location.lng)
+  end
+
+  def map_url
+    query = {
+      :center  => "#{ lat },#{ lng }",
+      :markers => "color:0x00AEEF|#{ lat },#{ lng }",
+      :zoom    => 13,
+      :size    => "500x200",
+      :scale   => 2,
+      :sensor  => false,
+      :style   => "saturation:-100"
+    }
+
+    #query_string = query.to_a.map{|kv| kv.join('=')}.join('&')
+    query_string = query.to_query
+
+    url = "http://maps.googleapis.com/maps/api/staticmap?" + query_string
+  end
+
   class Importer
 =begin
 The Importer expects rows to be an array of hash like objects that have
