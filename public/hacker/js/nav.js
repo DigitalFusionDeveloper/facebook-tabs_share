a0a12e47d363932d98be6d18df7e70b7498ec6f9
Miles Matthias <miles.matthias@gmail.com>
2013-10-29 16:19:26 -0600

    Removed some unused code and added comments to explain how the hacker tab selection works.

--- public/hacker/js/nav.js
+++ public/hacker/js/nav.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
     $(function() {
-    	$('#nav li').mouseover(
-                function() {
-        $('#nav li').addClass('inactive');
-        $(this).removeClass('inactive');
-    		var idx = $('#nav li').index(this);              
-        $('#details').html($('#descriptions div').eq(idx).html()).show();
-    		$img = $('#images img').eq(idx);
-    		$('#main_image').attr('src',$img.attr('src'));
-                }
-    	);
+        $('#nav li').mouseover(function() {
+            // inactivate all of the links
+            $('#nav li').addClass('inactive');
+            // activate the one the user is hovering over
+            $(this).removeClass('inactive');
+            // what's the index of the one the user is hovering over?
+            var idx = $('#nav li').index(this);
+            // get the html for details of the selected beer
+            var detailsHtml = $('#descriptions div').eq(idx).html();
+            // replace the visible details section with selected beer details html, ensure visible
+            $('#details').html(detailsHtml).show();
+            // @TODO - question: is it better to copy html and replace dom contents or do a css display none/block switch?
+        });
     });
+
+    // On page load, weisse beer should be selected
     $('#nav li').addClass('inactive');
     $('#nav li.weisse').removeClass('inactive');
     $('#details').html($('#descriptions div.weisse-deets').html()).show();
